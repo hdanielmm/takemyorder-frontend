83197e23263e6d0629229251925ede3000edd8d8
daniel <hdanielmm@gmail.com>
2019-11-27 13:32:32 -0500

    Add saveOrder in api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,6 +5,20 @@ export function getItems() {
   return axios.get('https://api.punkapi.com/v2/beers/');
 }
 
+export const saveOrder = ({ itemId, name, quantity }) => {
+  return axios({
+    method: 'post',
+    url: 'http://takemyorder-backend.herokuapp.com/pedidos',
+    data: {
+      itemId,
+      name,
+      quantity
+    }
+  })
+    .then(response => console.log(response))
+    .catch(error => console.log(error));
+}
+
 export function createItem({ name, price, image }) {
   return axios({
     method: 'post',
@@ -17,14 +31,14 @@ export function createItem({ name, price, image }) {
   });
 }
 
-export function getBeers(){
+export function getBeers() {
   return axios.get("http://takemyorder-backend.herokuapp.com/items");
 };
 
-export function getPedidos(){
+export function getPedidos() {
   return axios.get("http://takemyorder-backend.herokuapp.com/pedidos");
 };
 
-export function getMesas(){
+export function getMesas() {
   return axios.get("http://takemyorder-backend.herokuapp.com/mesas");
 };
